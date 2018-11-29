e75046e1c0aee1738de8be04214379ff06f5bc3a
Paul Isaichuk <pisaychuk@cybervisiontech.com>
2018-11-29 16:54:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Divider, Grid, Header, Icon } from 'semantic-ui-react'
+import './App.css'
+import AccordionExampleMenu from './components/AccordionExampleMenu'
+import SearchExampleStandard from './components/SearchExampleStandard'
+import MenuExamplePointing from './components/MenuExamplePointing'
+import CardExampleGroups from './components/CardExampleGroups'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <Grid celled>
+            <Grid.Row>
+              <Grid.Column width={3}>
+                Logo
+              </Grid.Column>
+              <Grid.Column width={11}>
+                <MenuExamplePointing>
+                </MenuExamplePointing>
+              </Grid.Column>
+              <Grid.Column width={2}>
+                <Icon name='cart' size='big' />
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <AccordionExampleMenu>
+                </AccordionExampleMenu>
+              </Grid.Column>
+              <Grid.Column width={11}>
+                <CardExampleGroups>
+                </CardExampleGroups>
+              </Grid.Column>
+              <Grid.Column width={2}>
+                banner
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column width={16}>
+                <Header textAlign='center'>
+                  <p className='inner'>Footer</p>
+                </Header>
+              </Grid.Column>
+            </Grid.Row>
+          
+          </Grid>
+        </Container>
       </div>
     );
   }
