b1bd163bff1fac83638d1092d01da66e74d0c83e
Paul Isaichuk <pisaychuk@cybervisiontech.com>
2018-11-30 19:36:27 +0200

    add fake data

--- src/components/CardExampleGroups.js
+++ src/components/CardExampleGroups.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Button, Card, Image, Icon } from 'semantic-ui-react'
 
 const CardExampleGroups = () => (
   <Card.Group>
@@ -7,7 +7,6 @@ const CardExampleGroups = () => (
       <Card.Content>
         <Image floated='right' size='mini' src='/images/avatar/large/steve.jpg' />
         <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
         <Card.Description>
           Steve wants to add you to the group <strong>best friends</strong>
         </Card.Description>
@@ -27,7 +26,6 @@ const CardExampleGroups = () => (
       <Card.Content>
         <Image floated='right' size='mini' src='/images/avatar/large/molly.png' />
         <Card.Header>Molly Thomas</Card.Header>
-        <Card.Meta>New User</Card.Meta>
         <Card.Description>
           Molly wants to add you to the group <strong>musicians</strong>
         </Card.Description>
@@ -44,16 +42,15 @@ const CardExampleGroups = () => (
       <Card.Content>
         <Image floated='right' size='mini' src='/images/avatar/large/jenny.jpg' />
         <Card.Header>Jenny Lawrence</Card.Header>
-        <Card.Meta>New User</Card.Meta>
         <Card.Description>Jenny requested permission to view your contact details</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className='ui two buttons'>
-          <Button basic color='green'>
-            Approve
-          </Button>
           <Button basic color='red'>
             Decline
+          </Button>
+          <Button basic color='green'>
+            <Icon name='cart arrow down'/>
           </Button>
         </div>
       </Card.Content>
@@ -62,7 +59,6 @@ const CardExampleGroups = () => (
       <Card.Content>
         <Image floated='right' size='mini' src='/images/avatar/large/jenny.jpg' />
         <Card.Header>Jenny Lawrence</Card.Header>
-        <Card.Meta>New User</Card.Meta>
         <Card.Description>Jenny requested permission to view your contact details</Card.Description>
       </Card.Content>
       <Card.Content extra>
